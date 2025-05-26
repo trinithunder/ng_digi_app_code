37fa3a9582016d7d1fe0729d557fe8f6d5c9f760
Marlon <marlon@Marlons-MBP.lan>
2025-05-26 18:36:10 -0400

    First

---
+++ NGDigital/NGDigital/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NGDigital
+//
+//  Created by Marlon on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NGDigital/NGDigital/NGDigitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NGDigitalApp.swift
+//  NGDigital
+//
+//  Created by Marlon on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NGDigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
